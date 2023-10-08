aef668b75e884653fdb745456ab089894df40ea1
skudlur <suhaskv0111@gmail.com>
2023-10-08 12:32:58 +0530

    Add: src dir

---
+++ src/cli.py
@@ -0,0 +1,11 @@
+#======================================================
+#	RISC-V Instruction Generator
+#	Testing your cores, sims and ems made simpler!
+#======================================================
+
+# Imports
+import os
+import click
+import sys
+
+# CLI
